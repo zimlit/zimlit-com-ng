3b1094dc7a19c5bd7925ef20562405931bc3316b
zimlit <zimlit2010@gmail.com>
2021-03-15 12:07:51 -0400

    heroku again

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
-//Install express server
 const express = require('express');
-const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+app.use(express.static('./dist/zimlit-com-ng'));
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/zimlit-com-ng/'}
+);
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
