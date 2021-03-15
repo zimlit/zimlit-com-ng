f6d533214f01d70a3a723dbf6f1bda9c72be3ff6
zimlit <zimlit2010@gmail.com>
2021-03-15 06:21:12 -0400

    home

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.styl']
+})
+export class HeaderComponent implements OnInit {
+
+  @Input() items?: string[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
