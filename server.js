449ad901589edc3090e860fd6d51c065b66688de
zimlit <zimlit2010@gmail.com>
2021-03-15 14:29:24 -0400

    redirect

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
-app.use(express.static('./dist/zimlit-com-ng'));
+
 app.use((req, res, next) => {
   if (reg.get('Host') == 'zimlit-com-ng.herokuapp.com') {
     return res.redirect(301, 'www.zimlit.com');
   }
   return next();
 })
+app.use(express.static('./dist/zimlit-com-ng'));
+
 app.get('/*', function(req, res) {
   res.sendFile('index.html', {root: 'dist/zimlit-com-ng/'}
 );
